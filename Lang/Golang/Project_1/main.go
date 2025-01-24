394cd17477b5fb93b59c47437c71d8e816fc8d34
Quix <quix.git@gmail.com>
2025-01-24 15:56:59 +0000

    1

--- Lang/Golang/Project_1/main.go
+++ Lang/Golang/Project_1/main.go
@@ -2,19 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 )
 
 func main() {
-	r := exec.Command("bash", "-c", `apt-get update && sudo apt-get upgrade && sudo apt-get install exa -y`)
-
-	output, err := r.CombinedOutput()
-	if err != nil {
-		fmt.Printf("Exeс error: %v\n", err)
-		fmt.Println(string(output))
-		return
-	}
-	fmt.Println(string(output))
 
 	fmt.Println("Обновленеи пакетов линукс завершено")
 }
